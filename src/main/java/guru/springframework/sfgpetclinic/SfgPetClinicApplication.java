a202fd48021c541f05576cd9cccbc7bc312dfac9
AmneetSinghh <amneet@vahan.co>
2021-09-08 15:37:36 +0545

    Pet-clinic

---
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		System.out.print("lets run this: ");
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+
+}
